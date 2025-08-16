630dc05d328731e686034d424ce34f2fbb828f51
Silver <silver@squirl.dev>
2025-08-16 04:59:57 +0100

    flake: switch to silversquirl/zig-flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,27 @@
 {
   inputs = {
-    zig.url = "github:mitchellh/zig-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    zig.inputs.nixpkgs.follows = "nixpkgs";
+    zls.inputs.nixpkgs.follows = "nixpkgs";
+
+    zig.url = "github:silversquirl/zig-flake";
     zls.url = "github:zigtools/zls";
-    # zls.inputs.zig-overlay.follows = "zig";
   };
 
-  outputs = inputs:
-    inputs.zig.inputs.flake-utils.lib.eachDefaultSystem (system: let
-      overlay = final: prev: {
-        zig = inputs.zig.packages.${prev.system}.master;
-        zls = inputs.zls.packages.${prev.system}.zls;
-      };
-
-      pkgs = import inputs.zig.inputs.nixpkgs {
-        inherit system;
-        overlays = [overlay];
-      };
-
-      # Shim to make zls work with latest nightly zig
-      zig-shim = pkgs.writeShellScriptBin "zig" ''
-        if [[ "$1" == "env" ]]; then
-          ${pkgs.zig}/bin/zig "$@" | z2j
-          exit
-        fi
-        exec ${pkgs.zig}/bin/zig "$@"
-      '';
+  outputs = inputs: let
+    forAllSystems = f: builtins.mapAttrs (system: f) inputs.nixpkgs.legacyPackages;
+  in {
+    devShells = forAllSystems (pkgs: let
+      zig = inputs.zig.packages.${pkgs.system}.nightly;
+      zls = inputs.zls.packages.${pkgs.system}.zls;
     in {
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [zig-shim zls];
+      default = pkgs.mkShellNoCC {
+        packages = [zig zls];
       };
+    });
 
-      packages.default = pkgs.stdenvNoCC.mkDerivation {
+    packages = forAllSystems (pkgs: {
+      default = pkgs.stdenvNoCC.mkDerivation {
         pname = "z2j";
         version = "0.1.0";
         src = with pkgs.lib.fileset;
@@ -45,13 +36,13 @@
           "--color"
           "off"
 
-          "-Dtarget=${system}"
+          "-Dtarget=${pkgs.system}"
           "-Dcpu=baseline"
         ];
         zigBuildFlags = ["--release=safe"];
         zigTestFlags = [];
 
-        nativeBuildInputs = [pkgs.zig];
+        nativeBuildInputs = [inputs.zig.packages.${pkgs.system}.nightly];
         buildPhase = ''
           runHook preBuild
           zig build --prefix "$out" \
@@ -70,10 +61,6 @@
           runHook postCheck
         '';
       };
-    })
-    // {
-      overlays.default = final: prev: {
-        z2j = inputs.self.packages.${prev.system}.default;
-      };
-    };
+    });
+  };
 }
